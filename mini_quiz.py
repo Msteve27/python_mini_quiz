52255ccc230ff200eb1acdfc807574cd2b848d0e
Msteve27 <msteve27@vols.utk.edu>
2022-05-18 14:13:34 -0500

    4 quiz questions finished

--- mini_quiz.py
+++ mini_quiz.py
@@ -1,15 +1,33 @@
-print("Welcome to my computer quiz! Built with python. Please use lower case letters only.")
+print("Welcome to my computer quiz! Built with python.")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
     
 print("OK, let's play! :) ")
 
 answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
     print("Correct!")
 else: 
-    print("Incorrect, please try agian ")
+    print("Incorrect")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+else: 
+    print("Incorrect")
     
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+else: 
+    print("Incorrect")
+
+answer = input("What does GCP stand for? ")
+if answer.lower() == "google cloud platform":
+    print("Correct!")
+else: 
+    print("Incorrect")
+   
